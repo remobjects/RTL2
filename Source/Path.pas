25a946996f92f4607793b05e0d972f509ac02e36
marc hoffman <mh@remobjects.com>
2017-01-08 08:39:55 -0400

    `Path` added more apis specific for Unix or Windows style paths

--- Source/Path.pas
+++ Source/Path.pas
@@ -4,26 +4,34 @@ interface
 
 type
   Path = public static class
+  private
+    method DoGetParentDirectory(aFileName: not nullable String; aFolderSeparator: Char): nullable String;
+
   public
     method ChangeExtension(aFileName: not nullable String; NewExtension: nullable String): not nullable String;
-    
+
     method Combine(aBasePath: not nullable String; params aPaths: array of String): not nullable String;
     method CombineUnixPath(aBasePath: not nullable String; params aPaths: array of String): not nullable String;
     method CombineWindowsPath(aBasePath: not nullable String; params aPaths: array of String): not nullable String;
 
     method GetParentDirectory(aFileName: not nullable String): nullable String;
+    method GetUnixParentDirectory(aFileName: not nullable String): nullable String;
+    method GetWindowsParentDirectory(aFileName: not nullable String): nullable String;
+
     method GetExtension(aFileName: not nullable String): not nullable String;
     method GetFileName(aFileName: not nullable String): not nullable String;
+    method GetUnixFileName(aFileName: not nullable String): not nullable String;
+    method GetWindowsFileName(aFileName: not nullable String): not nullable String;
     method GetFileNameWithoutExtension(aFileName: not nullable String): not nullable String;
     method GetPathWithoutExtension(aFileName: not nullable String): not nullable String;
     method GetFullPath(RelativePath: not nullable String): not nullable String;
-    
+
     method GetNetworkServerName(aFileName: not nullable String): nullable String;
-    
+
     {$IF TOFFEE}
     method ExpandTildeInPath(aPath: not nullable String): not nullable String;
     {$ENDIF}
-    
+
     property DirectorySeparatorChar: Char read Folder.Separator;
   end;
 
@@ -48,11 +56,11 @@ end;
 method Path.Combine(aBasePath: not nullable String; params aPaths: array of String): not nullable String;
 begin
   result := aBasePath;
-  if result.EndsWith(DirectorySeparatorChar) and (length(aPaths) > 0) then
+  if result.EndsWith(Folder.Separator) and (length(aPaths) > 0) then
     result := result.Substring(0, result.Length-1);
   for each p in aPaths do
     if length(p) > 0 then
-      result := result+DirectorySeparatorChar+p;
+      result := result+Folder.Separator+p;
 end;
 
 method Path.CombineUnixPath(aBasePath: not nullable String; params aPaths: array of String): not nullable String;
@@ -77,18 +85,33 @@ end;
 
 method Path.GetParentDirectory(aFileName: not nullable String): nullable String;
 begin
+  result := DoGetParentDirectory(aFileName, Folder.Separator);
+end;
+
+method Path.GetUnixParentDirectory(aFileName: not nullable String): nullable String;
+begin
+  result := DoGetParentDirectory(aFileName, '/');
+end;
+
+method Path.GetWindowsParentDirectory(aFileName: not nullable String): nullable String;
+begin
+  result := DoGetParentDirectory(aFileName, '\');
+end;
+
+method Path.DoGetParentDirectory(aFileName: not nullable String; aFolderSeparator: Char): nullable String;
+begin
   if length(aFileName) = 0 then
     raise new ArgumentException("Invalid arguments");
-    
+
   var LastChar := aFileName[aFileName.Length - 1];
 
-  if LastChar = Folder.Separator then
+  if LastChar = aFolderSeparator then
     aFileName := aFileName.Substring(0, aFileName.Length - 1);
 
-  if (aFileName = Folder.Separator) or ((length(aFileName) = 2) and (aFileName[1] = ':')) then
+  if (aFileName = aFolderSeparator) or ((length(aFileName) = 2) and (aFileName[1] = ':')) then
     exit nil; // root folder has no parent
 
-  var lIndex := aFileName.LastIndexOf(Folder.Separator);
+  var lIndex := aFileName.LastIndexOf(aFolderSeparator);
 
   if aFileName.StartsWith('\\') then begin
 
@@ -96,15 +119,15 @@ begin
       result := aFileName.Substring(0, lIndex)
     else
       result := nil; // network share has no parent folder
-    
+
   end
   else begin
-  
+
     if lIndex > -1 then
       result := aFileName.Substring(0, lIndex)
     else
       result := ""
-      
+
   end;
 end;
 
@@ -112,7 +135,7 @@ method Path.GetExtension(aFileName: not nullable String): not nullable String;
 begin
   aFileName := GetFileName(aFileName);
   var lIndex := aFileName.LastIndexOf(".");
-  
+
   if (lIndex <> -1) and (lIndex < aFileName.Length - 1) then
     exit aFileName.Substring(lIndex);
 
@@ -121,14 +144,24 @@ end;
 
 method Path.GetFileName(aFileName: not nullable String): not nullable String;
 begin
-  result := aFileName.GetLastPathComponentWithSeparatorChar(DirectorySeparatorChar);
+  result := aFileName.GetLastPathComponentWithSeparatorChar(Folder.Separator);
+end;
+
+method Path.GetUnixFileName(aFileName: not nullable String): not nullable String;
+begin
+  result := aFileName.GetLastPathComponentWithSeparatorChar('/');
+end;
+
+method Path.GetWindowsFileName(aFileName: not nullable String): not nullable String;
+begin
+  result := aFileName.GetLastPathComponentWithSeparatorChar('\');
 end;
 
 method Path.GetFileNameWithoutExtension(aFileName: not nullable String): not nullable String;
 begin
   aFileName := GetFileName(aFileName);
   var lIndex := aFileName.LastIndexOf(".");
-  
+
   if lIndex <> -1 then
     exit aFileName.Substring(0, lIndex);
 
@@ -138,8 +171,8 @@ end;
 method Path.GetPathWithoutExtension(aFileName: not nullable String): not nullable String;
 begin
   var lIndex := aFileName.LastIndexOf(".");
-  var lIndex2 := aFileName.LastIndexOf(DirectorySeparatorChar);
-  
+  var lIndex2 := aFileName.LastIndexOf(Folder.Separator);
+
   if (lIndex > lIndex2) then
     exit aFileName.Substring(0, lIndex);
 
@@ -149,7 +182,7 @@ end;
 method Path.GetFullPath(RelativePath: not nullable String): not nullable String;
 begin
   {$IF COOPER}
-  exit new java.io.File(RelativePath).AbsolutePath as not nullable;  
+  exit new java.io.File(RelativePath).AbsolutePath as not nullable;
   {$ELSEIF NETFX_CORE}
   exit RelativePath; //api has no such function
   {$ELSEIF WINDOWS_PHONE}
