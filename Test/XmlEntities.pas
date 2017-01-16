678991cb0438c42c866d6741fbd16cdf11a1b9a3
marc hoffman <mh@remobjects.com>
2017-01-16 11:20:17 -0400

    Tests

--- Test/XmlEntities.pas
+++ Test/XmlEntities.pas
@@ -10,9 +10,15 @@ type
     method Parsing;
     begin
       var xml := XmlDocument.FromString('<foo attr="test &amp; test"/>');
-      writeLn(xml);
+      Assert.AreEqual(xml.Root.Attribute["attr"].ToString, 'attr="test &amp; test"');
+      Assert.AreEqual(xml.Root.Attribute["attr"].Value, 'test & test');
+
+      var xml2 := XmlDocument.FromString('<foo attr="test &amp; &bad; & bad test" />');
+      Assert.AreEqual(xml2.Root.Attribute["attr"].Value, "test & &bad; & bad test");
+      Assert.AreEqual(xml2.Root.Attribute["attr"].ToString, 'attr="test &amp; &amp;bad; &amp; bad test"');
+      Assert.AreEqual(xml2, '<foo attr="test &amp; &amp;bad; &amp; bad test" />');
     end;
-    
+
   end;
 
 end.
