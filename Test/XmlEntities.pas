ab24123829c9bb55967dc2acb5fa7ca04c361ed3
marc hoffman <mh@remobjects.com>
2017-01-17 10:35:19 -0400

    More tests; fixed Echoes XML files

--- Test/XmlEntities.pas
+++ Test/XmlEntities.pas
@@ -17,6 +17,34 @@ type
       Assert.AreEqual(xml2.Root.Attribute["attr"].Value, "test & &bad; & bad test");
       Assert.AreEqual(xml2.Root.Attribute["attr"].ToString, 'attr="test &amp; &amp;bad; &amp; bad test"');
       Assert.AreEqual(xml2.ToString, '<foo attr="test &amp; &amp;bad; &amp; bad test" />');
+
+      var xml3 := XmlDocument.FromString('<Compile Include="$(MSBuildThisFileDirectory)TimeZone.pas"><VirtualFolder>Dates &amp; Co</VirtualFolder></Compile>');
+      Assert.AreEqual(xml3.Root.FirstElementWithName("VirtualFolder").Value, "Dates & Co");
+    end;
+
+    method Encoding;
+    begin
+      var xml := XmlDocument.FromString('<Test/>');
+      xml.Root.SetAttribute("Name", "Dates & Co");
+      Assert.AreEqual(xml.Root.Attribute["Name"].ToString, 'Name="Dates &amp; Co"');
+      xml.Root.SetAttribute("Name", "Dates &amp; Co");
+      Assert.AreEqual(xml.Root.Attribute["Name"].ToString, 'Name="Dates &amp;amp; Co"'); // do encode the &!
+
+      xml.Root.SetAttribute("Name", "don't you forget about me");
+      Assert.AreEqual(xml.Root.Attribute["Name"].ToString, 'Name="don''t you forget about me"'); // dont encode the '
+
+      xml.Root.SetAttribute("Name", 'and i said "what about ''breakfast at tiffany''s''?"');
+      Assert.AreEqual(xml.Root.Attribute["Name"].ToString, 'Name="and i said &quot;what about ''breakfast at tiffany''s''?&quot;'); // do encode the " but not the '
+
+      var xml3 := XmlDocument.FromString('<Compile ><VirtualFolder>x</VirtualFolder></Compile>');
+      xml3.Root.FirstElementWithName("VirtualFolder").Value := "Dates & Co";
+      Assert.AreEqual(xml3.Root.FirstElementWithName("VirtualFolder").ToString, "<VirtualFolder>Dates &amp; Co</VirtualFolder>");
+
+      xml3.Root.FirstElementWithName("VirtualFolder").Value := "Dates &amp; Co";
+      Assert.AreEqual(xml3.Root.FirstElementWithName("VirtualFolder").Value, "<VirtualFolder>Dates &amp;amp; Co</VirtualFolder>");
+
+      xml3.Root.FirstElementWithName("VirtualFolder").Value := 'and i said "what about ''breakfast at tiffany''s''?"';
+      Assert.AreEqual(xml3.Root.FirstElementWithName("VirtualFolder").Value, '<VirtualFolder>and i said "what about ''breakfast at tiffany''s''?"</VirtualFolder>'); // domt encode either quote
     end;
 
   end;
--- Test/XmlNamespaces.pas
+++ Test/XmlNamespaces.pas
@@ -10,7 +10,7 @@ type
     method Basics;
     begin
       var xml := XmlDocument.FromString('<?xml version="1.0" encoding="UTF-8" standalone="yes"?><root xmlns="http://default" xmlns:a="http://a" xmlns:b="http://bc"><one/><a:two/><b:three/></root>');
-      
+
       Assert.IsTrue(xml.Root.LocalName = "root");
       Assert.IsTrue(xml.Root.DefinedNamespaces.Count = 3);
       //Assert.IsTrue(xml.Root.DefaultNamespace.Name = "");
@@ -18,20 +18,27 @@ type
       Assert.AreEqual(xml.Root.DefinedNamespaces.ToList()[0].Url.ToAbsoluteString(), Url.UrlWithString("http://default").ToAbsoluteString());
       Assert.AreEqual(xml.Root.DefinedNamespaces.ToList()[1].Url.ToAbsoluteString(), Url.UrlWithString("http://a").ToAbsoluteString());
       Assert.AreEqual(xml.Root.DefinedNamespaces.ToList()[2].Url.ToAbsoluteString(), Url.UrlWithString("http://bc").ToAbsoluteString());
-    
+
       Assert.IsNotNil(xml.Root.FirstElementWithName("one"));
       Assert.IsNotNil(xml.Root.FirstElementWithName("two"));
       Assert.IsNotNil(xml.Root.FirstElementWithName("three"));
       Assert.IsNotNil(xml.Root.FirstElementWithName("a:two"));
       Assert.IsNotNil(xml.Root.FirstElementWithName("b:three"));
       Assert.IsNotNil(xml.Root.FirstElementWithName("{http://bc}three"));
-      
+
       //Assert.AreEqual(xml.Root.FirstElementWithName("one").Namespace, xml.Root.DefaultNamespace);
       Assert.AreEqual(xml.Root.FirstElementWithName("one").Namespace, xml.Root.DefinedNamespaces.ToList()[0]);
       Assert.AreEqual(xml.Root.FirstElementWithName("two"):&Namespace, xml.Root.DefinedNamespaces.ToList()[1]);
       Assert.AreEqual(xml.Root.FirstElementWithName("three"):&Namespace, xml.Root.DefinedNamespaces.ToList()[2]);
     end;
-    
+
+    method UserSettingsFile;
+    begin
+      var _userSettingsXML := XmlDocument.WithRootElement("Project");
+      _userSettingsXML.Root.AddNamespace(nil, Url.UrlWithString("http://schemas.microsoft.com/developer/msbuild/2003"));
+      Assert.AreEqual(_userSettingsXML.ToString, '<Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003"/>');
+    end;
+
   end;
 
 end.
