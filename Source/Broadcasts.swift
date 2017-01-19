3998015028d25bc20991087c1d7d1d37b0f20cfa
marc hoffman <mh@remobjects.com>
2017-01-19 09:29:34 -0400

    BroadcastManager improvements (fixes for properly unsubscribing on Cocoa

--- Source/Broadcasts.swift
+++ Source/Broadcasts.swift
@@ -27,20 +27,30 @@ public class Notification {
 
 public static class RemObjects.Elements.RTL.BroadcastManager {
 
-	#if !TOFFEE
-	private let subscriptions = Dictionary<String,List<(Object?,Block)>>()
+	#if TOFFEE
+	private let subscriptions = Dictionary<String,List<(Object,Object?,id)>>() // receiver, object, token
+	#else
+	private let subscriptions = Dictionary<String,List<(Object,Object?,Block)>>()	// receiver, block
 	#endif
 
 	//func subscribe(_ object: Object, toBroadcast broadcast: String, block: (Dictionary<String,Any>)->()) {
 	//}
 
-	public func subscribeToBroadcast(_ broadcast: String, block: (_ sender: Notification)->()) {
-		subscribeToBroadcast(broadcast, block: block, object: nil)
+	public func subscribe(_ receiver: Object, toBroadcast broadcast: String, block: (_ sender: Notification)->()) {
+		subscribe(receiver, toBroadcast: broadcast, block: block, object: nil)
 	}
 
-	public func subscribeToBroadcast(_ broadcast: String, block: (_ sender: Notification)->(), object: Object?) {
+	public func subscribe(_ receiver: Object, toBroadcast broadcast: String, block: (_ sender: Notification)->(), object: Object?) {
 		#if TOFFEE
-		NSNotificationCenter.defaultCenter.addObserver(for: broadcast, object: object, queue: nil, usingBlock: block);
+		let token = NSNotificationCenter.defaultCenter.addObserver(for: broadcast, object: object, queue: nil, usingBlock: block);
+		__lock self {
+			var subs = subscriptions[broadcast]
+			if subs == nil {
+				subs = List<(Object,Object?,id)>()
+				subscriptions[broadcast] = subs
+			}
+			subs!.Add((receiver, object, token))
+		}
 		#else
 		__lock self {
 			var subs = subscriptions[broadcast]
@@ -48,18 +58,18 @@ public static class RemObjects.Elements.RTL.BroadcastManager {
 				subs = List<(Object?,Block)>()
 				subscriptions[broadcast] = subs
 			}
-			subs!.Add((object, block))
+			subs!.Add((receiver, object, block))
 		}
 		#endif
 	}
 
-	public func subscribeToBroadcasts(_ broadcasts: List<String>, block: (_ sender: Notification)->()) {
-		subscribeToBroadcasts(broadcasts, block: block, object: nil)
+	public func subscribe(_ receiver: Object, toBroadcasts broadcasts: List<String>, block: (_ sender: Notification)->()) {
+		subscribe(receiver, toBroadcasts: broadcasts, block: block, object: nil)
 	}
 
-	public func subscribeToBroadcasts(_ broadcasts: List<String>, block: (_ sender: Notification)->(), object: Object?) {
+	public func subscribe(_ receiver: Object, toBroadcasts broadcasts: List<String>, block: (_ sender: Notification)->(), object: Object?) {
 		for b in broadcasts {
-			subscribeToBroadcast(b, block: block, object: object)
+			subscribe(receiver, toBroadcast: b, block: block, object: object)
 		}
 	}
 
@@ -69,13 +79,23 @@ public static class RemObjects.Elements.RTL.BroadcastManager {
 	}
 	#endif
 
-	public func unsubscribe(_ receiver: Object, fromBroadcast broadcast: String?) {
+	public func unsubscribe(_ receiver: Object, fromBroadcast broadcast: String) {
 		#if TOFFEE
 		NSNotificationCenter.defaultCenter.removeObserver(receiver, name: broadcast, object: nil)
+		__lock self {
+			if let subs = subscriptions[broadcast] {
+				for s in subs {
+					if s.0 == receiver {
+						NSNotificationCenter.defaultCenter.removeObserver(s.2)
+						subs.Remove(s)
+					}
+				}
+			}
+		}
 		#else
 		__lock self {
 			if let subs = subscriptions[broadcast] {
-				for s in subs? {
+				for s in subs {
 					if s.0 == receiver {
 						subs.Remove(s)
 					}
@@ -88,6 +108,16 @@ public static class RemObjects.Elements.RTL.BroadcastManager {
 	public func unsubscribe(_ receiver: Object) {
 		#if TOFFEE
 		NSNotificationCenter.defaultCenter.removeObserver(receiver)
+		__lock self {
+			for subs in subscriptions.Values {
+				for s in subs {
+					if s.0 == receiver {
+						NSNotificationCenter.defaultCenter.removeObserver(s.2)
+						subs.Remove(s)
+					}
+				}
+			}
+		}
 		#else
 		__lock self {
 			for k in subscriptions.Keys {
