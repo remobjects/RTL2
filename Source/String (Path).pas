a31ff45af316eee3d7417ecb276591da4f0d0611
marc hoffman <mh@remobjects.com>
2017-03-25 18:23:26 -0400

    `inline`s and performance tweaks

--- Source/String (Path).pas
+++ Source/String (Path).pas
@@ -16,7 +16,7 @@ type
     method GetLastWindowsPathComponent: String;
 
   assembly
-    method GetLastPathComponentWithSeparatorChar(aSeparator: Char): not nullable String;
+    method GetLastPathComponentWithSeparatorChar(aSeparator: String): not nullable String;
 
   public
 
@@ -28,9 +28,9 @@ type
     property LastUnixPathComponent: String read GetLastUnixPathComponent;       // always uses `/`
     property LastWindowsPathComponent: String read GetLastWindowsPathComponent; // always uses `\`
 
-    property PathWithoutExtension: String read Path.GetPathWithoutExtension(self);
-    property PathExtension: String read Path.GetExtension(self);
-    property NetworkServerName: nullable String read Path.GetNetworkServerName(self);
+    property PathWithoutExtension: String read Path.GetPathWithoutExtension(self); inline;
+    property PathExtension: String read Path.GetExtension(self); inline;
+    property NetworkServerName: nullable String read Path.GetNetworkServerName(self); inline;
 
     property IsWindowsPath: Boolean read (Length > 2) and ((self[1] = ':') or StartsWith("\\")); // Drive letter or Windows network path
 
@@ -72,7 +72,7 @@ type
 
 implementation
 
-method String.GetLastPathComponentWithSeparatorChar(aSeparator: Char): not nullable String;
+method String.GetLastPathComponentWithSeparatorChar(aSeparator: String): not nullable String;
 begin
   if RemObjects.Elements.System.length(self) = 0 then
     exit "";
