267a7dd26c358149f7b27aecc553a3525c5de195
marc hoffman <mh@remobjects.com>
2017-04-04 11:29:33 -0400

    Testcase cross-platform fix (not final)

--- Test/Stream.pas
+++ Test/Stream.pas
@@ -24,18 +24,18 @@ type
       Assert.AreEqual(lToRead[0], 9);
       lStream.Read(lToRead, 4);
       Assert.AreEqual(lToRead[0], 8);
-      
+
       var lNewStream := new MemoryStream(5);
       lStream.CopyTo(lNewStream);
-      
+
       lStream.Seek(2, SeekOrigin.Begin);
       Assert.AreEqual(lStream.Position, 2);
     end;
 
     method FileStreamTests;
     begin
-      var lPath := Environment.UserHomeFolder + '\tests';
-      var lStream := new FileStream(lPath, FileOpenMode.Create);      
+      var lPath := Path.Combine(Environment.UserHomeFolder, 'rtl2_tests');
+      var lStream := new FileStream(lPath, FileOpenMode.Create);
       var lArray := new Byte[5];
       var lToRead := new Byte[5];
       lArray := [9, 8, 7, 6, 5];
@@ -49,16 +49,16 @@ type
       Assert.AreEqual(lToRead[0], 9);
       lStream.Read(lToRead, 4);
       Assert.AreEqual(lToRead[0], 8);
-      
+
       var lNewStream := new MemoryStream(5);
       lStream.CopyTo(lNewStream);
-      
+
       lStream.Seek(2, SeekOrigin.Begin);
       Assert.AreEqual(lStream.Position, 2);
 
       lStream.Close;
       File.Delete(lPath);
     end;
-  end;    
+  end;
 
 end.
