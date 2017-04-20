2857d780c1ce9d68bc3b534eecffd57124eadb8f
marc hoffman <mh@remobjects.com>
2017-04-20 11:08:25 -0400

    Extension methods for .NET to make working with `.ToList().x*` easier w.o extra casts

---
+++ Source/List (Extensions).pas
@@ -0,0 +1,23 @@
+﻿namespace RemObjects.Elements.RTL;
+
+{$IF ECHOES}
+type
+  List_Extensions<T> = public extension class(System.Collections.Generic.List<T>)
+  private
+  protected
+  public
+
+    method ToSortedList: ImmutableList<T>;
+    begin
+      result := (self as ImmutableList<T>).ToSortedList();
+    end;
+
+    method UniqueCopy: not nullable ImmutableList<T>;
+    begin
+      result := (self as ImmutableList<T>).UniqueCopy();
+    end;
+
+  end;
+{$ENDIF}
+
+end.
