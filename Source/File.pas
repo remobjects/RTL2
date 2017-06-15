2992a7222fb62d441fd21166e24b063ad2a5c64d
marc hoffman <mh@remobjects.com>
2017-06-15 09:29:28 -0400

    Support for APFS copyfile() in Echoes/macOS (fixes)

--- Source/File.pas
+++ Source/File.pas
@@ -98,7 +98,7 @@ begin
     if aCloneIfPossible and (Environment.OS = OperatingSystem.macOS) and (Environment.macOS.IsHighSierraOrAbove) then begin
       if lNewFile.Exists then
         Delete(lNewFile);
-      if Foundation.copyfile(mapped, lNewFile, 0, Foundation.COPYFILE_CLONE or Foundation.COPYFILE_ALL) ≠ 0 then
+      if Foundation.copyfile(mapped, lNewFile, 0, Foundation.COPYFILE_CLONE) ≠ 0 then
         raise new RTLException("Failed to copy file");
     end
     else
