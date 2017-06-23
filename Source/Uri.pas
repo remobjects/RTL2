17d8dde1fec3b8763228fd04aa02dd80993ed15d
marc hoffman <mh@remobjects.com>
2017-06-23 07:49:18 -0400

    More flexible URN parsing

--- Source/Uri.pas
+++ Source/Uri.pas
@@ -8,18 +8,28 @@ type
 
     class method UriWithString(aUriString: not nullable String): Uri;
     begin
-      if aUriString:StartsWith("urn:") then
-        result := Urn.UrnWithString(aUriString)
+      var p := aUriString.IndexOf(":");
+      if p > -1 then begin
+        if aUriString.IndexOf("//", p+1) = 0 then
+          result := Url.TryUrlWithString(aUriString)
+        else
+          result := Urn.TryUrnWithString(aUriString);
+      end
       else
-        result := Url.UrlWithString(aUriString);
+        raise new UrlParserException("Not a valid URI, '{0}'", aUriString);
     end;
 
     class method TryUriWithString(aUriString: nullable String): Uri;
     begin
-      if aUriString:StartsWith("urn:") then
-        result := Urn.TryUrnWithString(aUriString)
-      else
-        result := Url.TryUrlWithString(aUriString);
+      if assigned(aUriString) then begin
+        var p := aUriString.IndexOf(":");
+        if p > -1 then begin
+          if aUriString.IndexOf("//", p+1) = 0 then
+            result := Url.TryUrlWithString(aUriString)
+          else
+            result := Urn.TryUrnWithString(aUriString);
+        end;
+      end;
     end;
 
   end;
