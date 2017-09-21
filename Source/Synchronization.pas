68bdbd8fbbb5505283f8fd62b866ca946c4ddb39
Diego Navarro <dinavarro@gmail.com>
2017-09-21 19:06:53 +0200

    Adding new Timer and Monitor classes for all platforms
    Working for Windows, testing other platforms

---
+++ Source/Synchronization.pas
@@ -0,0 +1,53 @@
+﻿namespace RemObjects.Elements.RTL;
+
+interface
+
+type
+  PlatformMonitor = {$IF COOPER}java.util.concurrent.locks.ReentrantLock{$ELSEIF ECHOES}System.Threading.ManualResetEvent{$ELSEIF ISLAND}Remobjects.Elements.System.Monitor{$ELSEIF TOFFEE}Foundation.NSRecursiveLock{$ENDIF};
+  
+  Monitor = public class mapped to PlatformMonitor
+  public
+    constructor;
+    method Lock;
+    method Unlock;
+  end;
+
+implementation
+
+constructor Monitor;
+begin
+  {$IF COOPER}
+  result := new java.util.concurrent.locks.ReentrantLock();
+  {$ELSEIF ECHOES}
+  result := new System.Threading.ManualResetEvent(true);
+  {$ELSEIF ISLAND}
+  result := new Remobjects.Elements.System.Monitor();
+  {$ELSEIF TOFFEE}
+  result := new Foundation.NSRecursiveLock();
+  {$ENDIF}
+end;
+
+method Monitor.Lock;
+begin
+  {$IF COOPER || TOFFEE}
+  mapped.lock;
+  {$ELSEIF ECHOES}
+  mapped.WaitOne;
+  {$ELSEIF ISLAND}
+  mapped.Wait;
+  {$ENDIF}
+end;
+
+method Monitor.Unlock;
+begin
+  {$IF COOPER OR TOFFEE}
+  mapped.unlock;
+  {$ELSEIF ECHOES}
+  mapped.Set;
+  {$ELSEIF ISLAND}
+  mapped.Release;
+  {$ENDIF}
+end;
+
+
+end.
