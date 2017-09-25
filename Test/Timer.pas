224d9ac01ea368f118603c9e26516c39859d0503
Diego Navarro <diegon@remobjects.com>
2017-09-25 17:59:33 +0200

    Adding basic Timer tests

---
+++ Test/Timer.pas
@@ -0,0 +1,40 @@
+﻿namespace RemObjects.Elements.RTL.Tests;
+
+uses
+  RemObjects.Elements.RTL,
+  RemObjects.Elements.EUnit;
+
+type
+  TimerTests = public class(Test)
+  private
+  protected
+  public
+    method TestBasicTimer;
+    begin
+      var lTimer := new RemObjects.Elements.RTL.Timer(100);
+      var lTest := 0;
+      lTimer.Elapsed := (aData) -> begin
+        inc(lTest, 1);
+      end;
+      lTimer.Start;
+      Thread.Sleep(350);
+      lTimer.Stop;
+      Assert.AreEqual(lTest, 3);
+    end;
+
+    method TestNoRepeatTimer;
+    begin
+      var lTimer := new RemObjects.Elements.RTL.Timer(100);
+      var lTest := 0;
+      lTimer.Repeat := false;
+      lTimer.Elapsed := (aData) -> begin
+        inc(lTest, 1);
+      end;
+      lTimer.Start;
+      Thread.Sleep(500);
+      lTimer.Stop;
+      Assert.AreEqual(lTest, 1);
+    end;
+  end;
+
+end.
