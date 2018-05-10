71eefe1bc46c37ee8544b6dbf7d19b23b58bc5ff
marc hoffman <mh@remobjects.com>
2018-05-10 07:49:16 -0400

    writeLn() overloads that takes a format string

--- Source/Functions.pas
+++ Source/Functions.pas
@@ -8,12 +8,27 @@ begin
     RemObjects.Elements.System.write(aMessage);
 end;
 
+method &write(aMessage: String); public; inline;
+begin
+  RemObjects.Elements.System.write(aMessage);
+end;
+
 method writeLn(aMessage: String; params aParams: array of Object); public; inline;
 begin
   if length(aParams) > 0 then
     RemObjects.Elements.System.writeLn(String.Format(aMessage, aParams))
   else
     RemObjects.Elements.System.writeLn(aMessage);
+end;
+
+method writeLn(aMessage: String); public; inline;
+begin
+  RemObjects.Elements.System.writeLn(aMessage);
+end;
+
+method writeLn(); public; inline;
+begin
+  RemObjects.Elements.System.writeLn();
 end;
 
 end.
