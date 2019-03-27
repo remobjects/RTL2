4a051e071f21d38656e1d18392c2389a2b746de2
marc hoffman <mh@remobjects.com>
2019-03-27 17:47:14 -0400

    Revert "Workaround for `82210: Toffee: "Const array of char" in mapped class is empty`"
    This reverts commit 30e68cb1f2f42509971d17d401bcc058d3e93c24.

--- Source/String.pas
+++ Source/String.pas
@@ -1117,15 +1117,15 @@ type
       //result := aString.GetSequence;
     //end;
 
-  end;
+    const
+      // from https://msdn.microsoft.com/en-us/library/system.Char.iswhitespace%28v=vs.110%29.aspx
+      WhiteSpaceCharacters: array of Char =
+      [#$0020, #$1680, #$2000, #$2001, #$2002, #$2003, #$2004, #$2005, #$2006, #$2007, #$2008, #$2009, #$200A, #$202F, #$205F, #$3000, //space separators
+       #$2028, //Line aSeparator
+       #$2029, //Paragraph aSeparator
+       #$0009, #$000A, #$000B, #$000C, #$000D,#$0085, #$00A0,  // other special symbols
+       #$FFEF]; public;
 
-  const
-    // from https://msdn.microsoft.com/en-us/library/system.Char.iswhitespace%28v=vs.110%29.aspx
-    WhiteSpaceCharacters: array of Char =
-    [#$0020, #$1680, #$2000, #$2001, #$2002, #$2003, #$2004, #$2005, #$2006, #$2007, #$2008, #$2009, #$200A, #$202F, #$205F, #$3000, //space separators
-     #$2028, //Line aSeparator
-     #$2029, //Paragraph aSeparator
-     #$0009, #$000A, #$000B, #$000C, #$000D,#$0085, #$00A0,  // other special symbols
-     #$FFEF]; public;
+  end;
 
 end.
