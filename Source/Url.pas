31a0e6e5b05a5278ba7b6d3279dcdf3008790293
marc hoffman <mh@remobjects.com>
2019-12-05 09:03:02 -0400

    NRE proofing in Url.LastPathComponentWithoutExtension (regression fix)

--- Source/Url.pas
+++ Source/Url.pas
@@ -107,7 +107,7 @@ type
 
     property PathExtension: String read GetPathExtension;     // will include the "."
     property LastPathComponent: String read GetLastPathComponent;
-    property LastPathComponentWithoutExtension: String read Path:LastPathComponentWithoutExtension;
+    property LastPathComponentWithoutExtension: String read GetLastPathComponent:PathWithoutExtension;
     property FilePathWithoutLastComponent: String read GetFilePathWithoutLastComponent;               // includes trailing "/" or "\"
     property WindowsPathWithoutLastComponent: String read GetWindowsPathWithoutLastComponent; // includes trailing "\"
     property UnixPathWithoutLastComponent: String read GetUnixPathWithoutLastComponent;       // includes trailing "/"
