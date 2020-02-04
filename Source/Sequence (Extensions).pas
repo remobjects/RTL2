0cef41d10bb7af1347836adfdd6b4893bca9cbf0
marc hoffman <mh@remobjects.com>
2020-02-04 16:07:28 -0400

    Sequence.JoinedString; Sequence.ToSortedList

--- Source/Sequence (Extensions).pas
+++ Source/Sequence (Extensions).pas
@@ -10,6 +10,37 @@ type
     //begin
       //result := &Select(i -> i as U).ToList();
     //end;
+
+    {$IF NOT ISLAND}
+    method ToSortedList: ImmutableList<T>;
+    begin
+      if self is ImmutableList<T> then
+        result := (self as ImmutableList<T>).ToSortedList()
+      else
+        result := self.ToList().ToSortedList();
+    end;
+    {$ENDIF}
+
+    method ToSortedList(aComparison: Comparison<T>): ImmutableList<T>;
+    begin
+      if self is ImmutableList<T> then
+        result := (self as ImmutableList<T>).ToSortedList(aComparison)
+      else
+        result := self.ToList().ToSortedList(aComparison)
+    end;
+
+
+    method JoinedString(aSeparator: nullable String := nil): not nullable String;
+    begin
+      var lResult := new StringBuilder();
+      for each e in self index i do begin
+        if (i ≠ 0) and assigned(aSeparator) then
+          lResult.Append(aSeparator);
+        lResult.Append(e:ToString());
+      end;
+      result := lResult.ToString() as not nullable;
+    end;
+
   end;
 {$ENDIF}
 
