c814c3b46821fc0250201dbbed369eb7eb41e25b
Carlo Kok <ck@remobjects.com>
2020-07-15 11:10:10 +0200

    0: Make extension methods public

--- Source/Object (Extensions).pas
+++ Source/Object (Extensions).pas
@@ -1,32 +1,32 @@
 ﻿namespace RemObjects.Elements.RTL;
 
 {$IF COOPER}
-extension method java.lang.Object.«ToString»: PlatformString;
+extension method java.lang.Object.«ToString»: PlatformString; public;
 begin
   result := «toString»();
 end;
 
-extension method java.lang.Object.«Equals»(aOther: Object): Boolean;
+extension method java.lang.Object.«Equals»(aOther: Object): Boolean;public;
 begin
   result := self.equals(aOther)
 end;
 
-extension method java.lang.Object.GetHashCode: Integer;
+extension method java.lang.Object.GetHashCode: Integer;public;
 begin
   result := hashCode;
 end;
 {$ELSEIF TOFFEE}
-extension method Foundation.NSObject.ToString: PlatformString;
+extension method Foundation.NSObject.ToString: PlatformString;public;
 begin
   result := description;
 end;
 
-extension method Foundation.NSObject.Equals(Obj: Object): Boolean;
+extension method Foundation.NSObject.Equals(Obj: Object): Boolean;public;
 begin
   result := isEqual(Obj);
 end;
 
-extension method Foundation.NSObject.GetHashCode: Integer;
+extension method Foundation.NSObject.GetHashCode: Integer;public;
 begin
   result := hash;
 end;
