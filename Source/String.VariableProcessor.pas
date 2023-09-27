bbd41c20a6975075e559a7cd724105f2f527e2f5
marc hoffman <mh@remobjects.com>
2023-09-27 15:20:00 -0400

    String.ProcessVariables

--- Source/String.VariableProcessor.pas
+++ Source/String.VariableProcessor.pas
@@ -6,19 +6,19 @@ type
   String = public partial class
   public
 
-    method ProcessVariables(aString: not nullable String; aStyle: VariableStyle; aCallback: block(aVariable: not nullable String): nullable String): not nullable String;
+    method ProcessVariables(aStyle: VariableStyle; aCallback: block(aVariable: not nullable String): nullable String): not nullable String;
     begin
       result := case aStyle of
-        VariableStyle.Percent: ProcessPlaceholders_PercentStyle(aString, aCallback);
-        VariableStyle.DoubleCurly: ProcessPlaceholders_DoubleCurlyStyle(aString, aCallback);
-        VariableStyle.EBuild: ProcessPlaceholders_EBuildStyle(aString, aCallback);
+        VariableStyle.Percent: ProcessPlaceholders_PercentStyle(aCallback);
+        VariableStyle.DoubleCurly: ProcessPlaceholders_DoubleCurlyStyle(aCallback);
+        VariableStyle.EBuild: ProcessPlaceholders_EBuildStyle(aCallback);
         else raise new Exception($"Unknown variable style {aStyle}");
       end;
     end;
 
   private
 
-    method ProcessPlaceholders_EBuildStyle(aString: not nullable String; aCallback: block(aVariable: not nullable String): nullable String): not nullable String;
+    method ProcessPlaceholders_EBuildStyle(aCallback: block(aVariable: not nullable String): nullable String): not nullable String;
     begin
 
       var lCurrentStart := 0;
@@ -33,25 +33,25 @@ type
       end;
 
       var i: Integer := 0;
-      var len := aString.Length;
+      var len := self.Length;
       Outer: while i < len-2 do begin
 
-        if aString[i] = '$' then begin
-          if aString[i+1] = '(' then begin
-            if (i > 0) and (aString[i-1] = '$') then begin // use `$$(` to escape, keep `$(`
-              AppendResult(aString.Substring(lCurrentStart, i-lCurrentStart));
+        if self[i] = '$' then begin
+          if self[i+1] = '(' then begin
+            if (i > 0) and (self[i-1] = '$') then begin // use `$$(` to escape, keep `$(`
+              AppendResult(self.Substring(lCurrentStart, i-lCurrentStart));
               AppendResult("(");
               inc(i, 2);
               lCurrentStart := i;
               continue Outer;
             end;
 
-            AppendResult(aString.Substring(lCurrentStart, i-lCurrentStart));
+            AppendResult(self.Substring(lCurrentStart, i-lCurrentStart));
             lCurrentStart := i;
             inc(i,2);
-            while i < aString.Length do begin
-              if aString[i] = ')' then begin
-                var lVariableName := aString.Substring(lCurrentStart+2, i-lCurrentStart-2);
+            while i < self.Length do begin
+              if self[i] = ')' then begin
+                var lVariableName := self.Substring(lCurrentStart+2, i-lCurrentStart-2);
                 var lValue := aCallback(lVariableName);
                 if assigned(lValue) then begin
                   AppendResult(lValue);
@@ -70,12 +70,12 @@ type
 
       end;
       if assigned(lResult) and (lCurrentStart < len) then
-        AppendResult(aString.Substring(lCurrentStart));
+        AppendResult(self.Substring(lCurrentStart));
 
-      result := coalesce(lResult:ToString, aString);
+      result := coalesce(lResult:ToString, self);
     end;
 
-    method ProcessPlaceholders_DoubleCurlyStyle(aString: not nullable String; aCallback: block(aVariable: not nullable String): nullable String): not nullable String;
+    method ProcessPlaceholders_DoubleCurlyStyle(aCallback: block(aVariable: not nullable String): nullable String): not nullable String;
     begin
 
       var lCurrentStart := 0;
@@ -90,25 +90,25 @@ type
       end;
 
       var i: Integer := 0;
-      var len := aString.Length;
+      var len := self.Length;
       Outer: while i < len-3 do begin
 
-        if aString[i] = '{' then begin
-          if aString[i+1] = '{' then begin
-            if aString[i+2] = '{' then begin // use `{{{` to escape, keep two
-              AppendResult(aString.Substring(lCurrentStart, i-lCurrentStart+2));
+        if self[i] = '{' then begin
+          if self[i+1] = '{' then begin
+            if self[i+2] = '{' then begin // use `{{{` to escape, keep two
+              AppendResult(self.Substring(lCurrentStart, i-lCurrentStart+2));
               inc(i, 3);
               lCurrentStart := i;
               continue Outer;
             end;
 
-            AppendResult(aString.Substring(lCurrentStart, i-lCurrentStart));
+            AppendResult(self.Substring(lCurrentStart, i-lCurrentStart));
             lCurrentStart := i;
             inc(i,2);
-            while i < aString.Length-1 do begin
-              if aString[i] = '}' then begin
-                if aString[i+1] = '}' then begin
-                  var lVariableName := aString.Substring(lCurrentStart+2, i-lCurrentStart-2);
+            while i < self.Length-1 do begin
+              if self[i] = '}' then begin
+                if self[i+1] = '}' then begin
+                  var lVariableName := self.Substring(lCurrentStart+2, i-lCurrentStart-2);
                   var lValue := aCallback(lVariableName);
                   if assigned(lValue) then begin
                     AppendResult(lValue);
@@ -126,12 +126,12 @@ type
 
       end;
       if assigned(lResult) and (lCurrentStart < len) then
-        AppendResult(aString.Substring(lCurrentStart));
+        AppendResult(self.Substring(lCurrentStart));
 
-      result := coalesce(lResult:ToString, aString);
+      result := coalesce(lResult:ToString, self);
     end;
 
-    method ProcessPlaceholders_PercentStyle(aString: not nullable String; aCallback: block(aVariable: not nullable String): nullable String): not nullable String;
+    method ProcessPlaceholders_PercentStyle(aCallback: block(aVariable: not nullable String): nullable String): not nullable String;
     begin
 
       var lCurrentStart := 0;
@@ -146,23 +146,23 @@ type
       end;
 
       var i: Integer := 0;
-      var len := aString.Length;
+      var len := self.Length;
       Outer: while i < len-2 do begin
 
-        if aString[i] = '%' then begin
-          if aString[i+1] = '%' then begin // use `%%` to escape, keep one
-            AppendResult(aString.Substring(lCurrentStart, i-lCurrentStart+1));
+        if self[i] = '%' then begin
+          if self[i+1] = '%' then begin // use `%%` to escape, keep one
+            AppendResult(self.Substring(lCurrentStart, i-lCurrentStart+1));
             inc(i, 2);
             lCurrentStart := i;
             continue Outer;
           end;
 
-          AppendResult(aString.Substring(lCurrentStart, i-lCurrentStart));
+          AppendResult(self.Substring(lCurrentStart, i-lCurrentStart));
           lCurrentStart := i;
           inc(i);
-          while i < aString.Length do begin
-            if aString[i] = '%' then begin
-              var lVariableName := aString.Substring(lCurrentStart+1, i-lCurrentStart-1);
+          while i < self.Length do begin
+            if self[i] = '%' then begin
+              var lVariableName := self.Substring(lCurrentStart+1, i-lCurrentStart-1);
               var lValue := aCallback(lVariableName);
               if assigned(lValue) then begin
                 AppendResult(lValue);
@@ -178,9 +178,9 @@ type
 
       end;
       if assigned(lResult) and (lCurrentStart < len) then
-        AppendResult(aString.Substring(lCurrentStart));
+        AppendResult(self.Substring(lCurrentStart));
 
-      result := coalesce(lResult:ToString, aString);
+      result := coalesce(lResult:ToString, self);
     end;
 
   end;
