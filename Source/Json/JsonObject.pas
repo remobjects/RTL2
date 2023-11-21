3ccd8b8e07dee36ae94d5dca746dd8b621482a35
marc hoffman <mh@remobjects.com>
2023-11-21 15:03:34 -0400

    Json: converted to unified Syntax

--- Source/Json/JsonObject.pas
+++ Source/Json/JsonObject.pas
@@ -1,11 +1,97 @@
 ﻿namespace RemObjects.Elements.RTL;
 
-interface
-
 type
   JsonObject = public class(JsonNode)
+  public
+
+    constructor;
+    begin
+      fItems := new Dictionary<String, JsonNode>();
+    end;
+
+    constructor(aItems: Dictionary<String, JsonNode>);
+    begin
+      fItems := aItems;
+    end;
+
+    //
+
+    method &Add(aKey: not nullable String; aValue: not nullable JsonNode);
+    begin
+      fItems[aKey] := aValue;
+    end;
+
+    method &Remove(aKey: not nullable String): Boolean;
+    begin
+      exit fItems.Remove(aKey);
+    end;
+
+    method Clear;
+    begin
+      fItems.RemoveAll;
+    end;
+
+    // will return false for non-existent keys and for JsonNullValue!
+    method ContainsKey(aKey: not nullable String): Boolean;
+    begin
+      var lValue := fItems[aKey];
+      exit assigned(lValue) and (lValue is not JsonNullValue);
+    end;
+
+    // will return false for non-existent keys and values other than JsonNullValue!
+    method ContainsExplicitJsonNullValueForKey(aKey: not nullable String): Boolean;
+    begin
+      exit fItems[aKey] is JsonNullValue;
+    end;
+
+    //
+
+    method ToString(aFormat: JsonFormat): String; override;
+    begin
+      var Serializer := new JsonSerializer(self, aFormat);
+      result := Serializer.Serialize;
+    end;
+
+    {$IF NOT TOFFEE}[&Sequence]{$ENDIF}
+    method GetSequence: sequence of tuple of (String, JsonNode); iterator;
+    begin
+      for each kv in fItems do
+        yield (kv.Key, kv.Value);
+    end;
+
+    //{$IF TOFFEE AND NOT TOFFEEV2}
+    //method countByEnumeratingWithState(aState: ^NSFastEnumerationState) objects(stackbuf: ^tuple of (String,JsonNode)) count(len: NSUInteger): NSUInteger;
+    //begin
+      //if aState^.state <> 0 then
+        //exit 0;
+      //exit GetProperties.countByEnumeratingWithState(aState) objects(stackbuf) count(len);
+    //end;
+    //{$ENDIF}
+
+    class method Load(JsonString: String): JsonObject;
+    begin
+      var Serializer := new JsonDeserializer(JsonString);
+      var lValue := Serializer.Deserialize;
+
+      if not (lValue is JsonObject) then
+        raise new JsonParserException("String does not contains valid Json object");
+
+      result := lValue as JsonObject;
+    end;
+
+    property Count: Integer read fItems.Count; override;
+    property Item[aKey: not nullable String]: nullable JsonNode read GetItem write SetItem; default; override;
+    property Item[aKey: not nullable String]: nullable String write SetItem; default; override;
+    property Item[aKey: not nullable String]: Boolean write SetItem; default; override;
+    property Item[aKey: not nullable String]: Int32 write SetItem; default; override;
+    property Item[aKey: not nullable String]: Double write SetItem; default; override;
+    property Keys: not nullable sequence of String read GetKeys; override;
+    property Properties: sequence of tuple of (String,JsonNode) read GetProperties;
+
   private
+
     fItems: Dictionary<String, JsonNode>;
+
     method GetItem(aKey: not nullable String): nullable JsonNode;
     begin
       if fItems.ContainsKey(aKey) then begin
@@ -51,90 +137,6 @@ type
         yield (aKey, Item[aKey]);
     end;
 
-  public
-    constructor;
-    begin
-      fItems := new Dictionary<String, JsonNode>();
-    end;
-
-    constructor(aItems: Dictionary<String, JsonNode>);
-    begin
-      fItems := aItems;
-    end;
-
-
-    method &Add(aKey: not nullable String; aValue: not nullable JsonNode);
-    begin
-      fItems[aKey] := aValue;
-    end;
-
-    method &Remove(aKey: not nullable String): Boolean;
-    begin
-      exit fItems.Remove(aKey);
-    end;
-
-    method Clear;
-    begin
-      fItems.RemoveAll;
-    end;
-
-    method ContainsKey(aKey: not nullable String): Boolean;
-    begin
-      var lValue := fItems[aKey];
-      exit assigned(lValue) and (lValue is not JsonNullValue);
-    end;
- // will return false for non-exist-ent keys and for JsonNullValue!
-    method ContainsExplicitJsonNullValueForKey(aKey: not nullable String): Boolean;
-    begin
-      exit fItems[aKey] is JsonNullValue;
-    end;
- // will return false for non-exist-ent keys and values other than JsonNullValue!
-
-    method ToString(aFormat: JsonFormat): String; override;
-    begin
-      var Serializer := new JsonSerializer(self, aFormat);
-      result := Serializer.Serialize;
-    end;
-
-
-    {$IF NOT TOFFEE}[&Sequence]{$ENDIF}
-    method GetSequence: sequence of tuple of (String, JsonNode); iterator;
-    begin
-      for each kv in fItems do
-        yield (kv.Key, kv.Value);
-    end;
-
-    //{$IF TOFFEE AND NOT TOFFEEV2}
-    //method countByEnumeratingWithState(aState: ^NSFastEnumerationState) objects(stackbuf: ^tuple of (String,JsonNode)) count(len: NSUInteger): NSUInteger;
-    //begin
-      //if aState^.state <> 0 then
-        //exit 0;
-      //exit GetProperties.countByEnumeratingWithState(aState) objects(stackbuf) count(len);
-    //end;
-    //{$ENDIF}
-
-    class method Load(JsonString: String): JsonObject;
-    begin
-      var Serializer := new JsonDeserializer(JsonString);
-      var lValue := Serializer.Deserialize;
-
-      if not (lValue is JsonObject) then
-        raise new JsonParserException("String does not contains valid Json object");
-
-      result := lValue as JsonObject;
-    end;
-
-
-    property Count: Integer read fItems.Count; override;
-    property Item[aKey: not nullable String]: nullable JsonNode read GetItem write SetItem; default; override;
-    property Item[aKey: not nullable String]: nullable String write SetItem; default; override;
-    property Item[aKey: not nullable String]: Boolean write SetItem; default; override;
-    property Item[aKey: not nullable String]: Int32 write SetItem; default; override;
-    property Item[aKey: not nullable String]: Double write SetItem; default; override;
-    property Keys: not nullable sequence of String read GetKeys; override;
-    property Properties: sequence of tuple of (String,JsonNode) read GetProperties;
   end;
-
-implementation
 
 end.
