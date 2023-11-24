b13d41dfcd6a94b443a2e697de11d0e2c647d413
marc hoffman <mh@remobjects.com>
2023-11-24 09:35:14 -0400

    Stopwatch class

---
+++ Source/Stopwatch.pas
@@ -0,0 +1,29 @@
+﻿namespace RemObjects.Elements.RTL;
+
+type
+  Stopwatch = public class
+  public
+
+    constructor;
+    begin
+      Start;
+    end;
+
+    method Start;
+    begin
+      StartTime := DateTime.UtcNow;
+    end;
+
+    property ElapsedTime: Double read (DateTime.UtcNow-StartTime).TotalMilliSeconds;
+    property StartTime: DateTime read private write;
+
+    [ToString]
+    method ToString: String; override;
+    begin
+      result := Convert.MillisecondsToTimeString(ElapsedTime);
+
+    end;
+
+  end;
+
+end.
