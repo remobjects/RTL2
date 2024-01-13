7b7930a4301cd5fc405c569096ffd80b8ebee67a
marc hoffman <mh@remobjects.com>
2024-01-13 08:52:39 -0400

    Arrays: equality operator, where applicable

---
+++ Source/Array+Extensions.pas
@@ -0,0 +1,28 @@
+﻿namespace RemObjects.Elements.RTL;
+
+type
+  ArrayExtensions<T> = public extension record(array of T) where T is IEquatable<T>;
+  public
+
+    operator &Equal(lhs: array of Byte; rhs: array of Byte): Boolean;
+    begin
+      {$IF NOT TOFFEE}
+      if Object(lhs) = Object(rhs) then
+        exit true;
+      {$ENDIF}
+      if RemObjects.Elements.System.length(lhs) ≠ RemObjects.Elements.System.length(rhs) then
+        exit false;
+      for i := 0 to RemObjects.Elements.System.length(lhs)-1 do
+        if lhs[i] ≠ rhs[i] then
+          exit false;
+      result := true;
+    end;
+
+    operator NotEqual(lhs: array of Byte; rhs: array of Byte): Boolean;
+    begin
+      result := not (lhs = rhs);
+    end;
+
+  end;
+
+end.
