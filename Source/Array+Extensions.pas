2aa87ce045371aa477b2f8660a18df256fa292dc
marc hoffman <mh@remobjects.com>
2024-01-23 10:50:28 -0400

    Arrays: equality operator, where applicable (fix for compiler fix)

--- Source/Array+Extensions.pas
+++ Source/Array+Extensions.pas
@@ -4,7 +4,7 @@ type
   ArrayExtensions<T> = public extension record(array of T) where T is IEquatable<T>;
   public
 
-    operator &Equal(lhs: array of Byte; rhs: array of Byte): Boolean;
+    operator &Equal(lhs: array of T; rhs: array of T): Boolean;
     begin
       {$IF NOT TOFFEE}
       if Object(lhs) = Object(rhs) then
@@ -12,13 +12,18 @@ type
       {$ENDIF}
       if RemObjects.Elements.System.length(lhs) ≠ RemObjects.Elements.System.length(rhs) then
         exit false;
-      for i := 0 to RemObjects.Elements.System.length(lhs)-1 do
-        if lhs[i] ≠ rhs[i] then
+      for i := 0 to RemObjects.Elements.System.length(lhs)-1 do begin
+        if not assigned(lhs[i]) and not assigned(rhs[i]) then
+          continue;
+        if not assigned(lhs[i]) or not assigned(rhs[i]) then
           exit false;
+        if not lhs[i].Equals(rhs[i]) then
+          exit false;
+      end;
       result := true;
     end;
 
-    operator NotEqual(lhs: array of Byte; rhs: array of Byte): Boolean;
+    operator NotEqual(lhs: array of T; rhs: array of T): Boolean;
     begin
       result := not (lhs = rhs);
     end;
