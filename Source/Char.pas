7792d54d60db375f18e2e96a32872c1bcc4cdee3
marc hoffman <mh@remobjects.com>
2024-10-21 15:41:04 -0400

    Char.LowercaseLatinLetters/UppercaseLatinLetters/Numbers const arrays

--- Source/Char.pas
+++ Source/Char.pas
@@ -157,6 +157,10 @@ type
       {$ENDIF}
     end;
 
+    const LowercaseLatinLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const UppercaseLatinLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    const Numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
   end;
 
   CharArrayExtensions = public extension record(array of Char)
